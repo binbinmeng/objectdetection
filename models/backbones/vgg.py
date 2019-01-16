4190c425682d263f94cdec763e0750e9e01c0c08
binbinmeng <15221657504@163.com>
2019-01-16 16:29:11 +0800

    add model

---
+++ models/backbones/vgg.py
@@ -0,0 +1,121 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+
+__all__ = [
+    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
+    'vgg19_bn', 'vgg19',
+]
+
+
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+
+class VGG(nn.Module):
+
+    def __init__(self, features,classify=True, num_classes=1000, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+
+        self.classifier_flag = classify
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+
+        if self.classifier_flag:
+            x = x.view(x.size(0), -1)
+            x = self.classifier(x)
+
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    11: [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    13: [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    16: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    19: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def VGGNet(depth, classify=True, pretrained=False, **kwargs):
+    """VGG 11-layer model (configuration "11")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    if depth == 11:
+        model = VGG(make_layers(cfg[depth]), classify, **kwargs)
+        if pretrained:
+            model.load_state_dict(model_zoo.load_url(model_urls['vgg11']))
+    if depth == 13:
+        model = VGG(make_layers(cfg[depth]), classify, **kwargs)
+        if pretrained:
+            model.load_state_dict(model_zoo.load_url(model_urls['vgg13']))
+    if depth == 16:
+        model = VGG(make_layers(cfg[depth]), classify, **kwargs)
+        if pretrained:
+            model.load_state_dict(model_zoo.load_url(model_urls['vgg16']))
+    if depth == 19:
+        model = VGG(make_layers(cfg[depth]), classify, **kwargs)
+        if pretrained:
+            model.load_state_dict(model_zoo.load_url(model_urls['vgg19']))
+
+    return model
+
+
+if __name__ == '__main__':
+    model = VGGNet(13,#depth
+                False #don't use classfier, just as base model
+                )
+    from torchsummary import summary
+    summary(model.cuda(),(3,224,224))
