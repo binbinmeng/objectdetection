5694022f67b8e765fb7f959efeeaaade5613c714
binbinmeng <15221657504@163.com>
2019-01-16 15:53:31 +0800

    delete

--- models/backbones/__init__.py
+++
@@ -1,2 +0,0 @@
-from .resnet import ResNet
-from .mobilenet import MobileNetV1, MobileNetV2
--- models/unit_tests/test_resnet.py
+++
@@ -1,7 +0,0 @@
-from backbones import *
-from torchsummary import summary
-
-if __name__ == '__main__':
-    model =ResNet(152)
-    model =model.cuda()
-    summary(model, (3,224,224))
