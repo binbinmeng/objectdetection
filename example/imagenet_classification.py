935c4b0d1b8c108a3495ebca2a721401477caaa8
binbinmeng <15221657504@163.com>
2019-01-22 02:59:13 +0000

    add

---
+++ example/imagenet_classification.py
@@ -0,0 +1,20 @@
+
+import argparse
+import os
+import shutil
+import time
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import backbone as models
+
+
